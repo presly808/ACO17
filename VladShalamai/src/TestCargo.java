92a95fc1195d7ae61f734697dad8506d50743578
Влад <shalamay.vlad44@gmail.com>
2016-11-19 12:40:16 +0200

    test for go() and go(int km)

---
+++ VladShalamai/src/TestCargo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Влад on 19.11.2016.
+ */
+public class TestCargo {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.init("bmw", 10, 5000, true);
+        int expFuel1 = 9;
+        int expFuel2 = 7;
+        car.go();
+        System.out.println("method go() is " + (car.getFuel() == expFuel1) );
+        car.go(200);
+        System.out.println("method go(int km) is " + (car.getFuel() == expFuel2) );
+
+    }
+}
