e7b3be01ba1ca13d8eb45f649dc3c20e029ebce2
Victoria <viktorialvov@gmail/com>
2016-11-19 18:16:11 +0200

    Car class with methods go, open, close, showinfo

---
+++ VictoriaGitina/src/Car.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * Created by Victoria on 11/19/2016.
+ */
+public class Car {
+    public String model;
+    public int fuel = 0;
+     public int price;
+    public boolean opened = true;
+    public int keyOpen = 1111;
+    public int keyClose = 1111;
+
+    public boolean open(int key) {
+
+        if (key == keyOpen) {
+            opened = true;
+        } else {
+            opened = false;
+        }
+        return opened;
+    }
+
+    public boolean close(int key) {
+
+        if (key == keyClose) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean go() {
+        if (fuel < 20) {
+            return false;
+        }
+        return true;
+    }
+
+    public String showInfo() {
+        String str  = new String ("");
+        //return fuel + " " + model;
+                str =  String.format("fuel %d, price %d, model %s, keyOpen %s",  fuel, price,  model, keyOpen, "keyClose %s", keyClose);
+        System.out.println (str);
+
+
+        return str;
+    }
+
+}
+
