3a703ef2fbebaf76f953288e80148873df12419c
goosik <gooseva@mailpd.com>
2016-11-19 19:29:48 +0200

    Update  Method go.

--- VikiGolieusova/src/week1/Car.java
+++ VikiGolieusova/src/week1/Car.java
@@ -3,22 +3,29 @@ package week1;
 public class Car {
 
     public String model;
+
+    private String originalKey;
     public boolean opened;
 
-    public int fuel;
+    public double fuel;
+    public int consumption;
 
     public double price;
-    private String originalKey;
 
-    public void init(String model, String originalKey, int fuel, double price) {
+
+
+
+    public void init(String model, String originalKey, double fuel, int consumption, double price ) {
         this.model = model;
         this.originalKey = originalKey;
         this.fuel = fuel;
         this.price = price;
+        this.consumption = consumption;
+
     }
 
     public boolean open(String key) {
-        if (key.equals(originalKey)) {
+        if (originalKey.equals(key)) {
             return opened = true;
         }
         return opened;
@@ -28,6 +35,18 @@ public class Car {
         if (car == null) {
             return null;
         }
-        return String.format("model %s, fuel %d, price %.2f", car.model, car.fuel, car.price);
+
+        return String.format("model %s, fuel %.2f, consumption %d, price %.2f", car.model, car.fuel,car.consumption, car.price);
+    }
+
+    public double go(int km) {
+        if (opened && fuel == 0) {
+            return 0;
+        } else if(fuel<(km / (100 / consumption))){
+            return 0;
+        }
+        fuel = fuel - (km / (100 / consumption));
+
+        return fuel;
     }
 }
