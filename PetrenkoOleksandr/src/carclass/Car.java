fab0de92dfd6387cc81171440457911c715ddd5d
Fr69za <Fr69Za@gmail.com>
2016-11-19 22:25:59 +0200

    PetrenkoCar

---
+++ PetrenkoOleksandr/src/carclass/Car.java
@@ -0,0 +1,44 @@
+package carclass;
+
+/**
+ * Created by Alexandr on 19.11.2016.
+ */
+public class Car {
+
+    public String model;
+    public float fuel;
+    public int prise;
+    public int trueKey = 123;
+
+
+    public boolean opened(int key) {
+        boolean rigth = false;
+        if (key == trueKey) {
+            rigth = true;
+        }
+        return rigth;
+    }
+
+    public boolean closed(int key) {
+        boolean rigth = false;
+        if (key == trueKey) {
+            rigth = true;
+        }
+        return rigth;
+    }
+
+    public String showInfo(){
+        return model + " ," + fuel + " ," + prise  + " .";
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
