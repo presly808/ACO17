ab7adcd15a85f64d6ed616ffcf6188d728f25c77
Влад <shalamay.vlad44@gmail.com>
2016-11-19 12:08:07 +0200

    create field

---
+++ VladShalamai/src/Car.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Влад on 19.11.2016.
+ */
+public class Car {
+    String model;
+    int fuel;
+    double price;
+    boolean opened;
+}
--- VladShalamai/src/VladTest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Влад on 19.11.2016.
- */
-public class VladTest {
-}
