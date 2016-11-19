96b241ea2ac5cdfe2a85cf8e0afd02e3f6d81da6
QAtesterQA <pan-_-roman@i.ua>
2016-11-19 19:44:30 +0200

    CarTask

--- RomanAvdieiev/src/avdieiev/week1/Car.java
+++ RomanAvdieiev/src/avdieiev/week1/Car.java
@@ -8,20 +8,48 @@ public class Car {
     public double fuel;
     public double price;
     public String originKey;
+    public double consumption;
 
-    public void init(String model, boolean opened, double fuel, double price,String originKey) {
+
+    public void init(String model, boolean opened, double fuel, double price, String originKey, double consumption) {
         Car car = this;
+        // this.model = model;
         car.model = model;
         car.opened = opened;
         car.fuel = fuel;
         car.price = price;
         car.originKey = originKey;
+        this.consumption = consumption;
+
     }
 
-    public boolean open(String key){
-        if (originKey.equals(opened)){
+    public boolean open(String key) {
+        if (originKey.equals(opened)) {
             opened = true;
         }
         return opened;
+    }
+
+    public String ShowAll(Car car) {
+        if (car == null) {
+            return null;
+        }
+        return "Model - " + model + "; " + " Opened - " + opened + "; " + " Fuel - " + fuel + "; " +
+                " Price - " + price + "; " + "Consumption " + consumption;
+        //    return String.format("model %s, fuel %d, price %.2f", car.model, car.fuel, car.price);
+    }
+
+    public double go(int km) {
+        if (fuel == 0) {
+            return 0;
+        } else if (opened == false){
+            return 0;
+        }
+        double consumption1 = 100 / consumption;
+        fuel = fuel - (km / consumption1);
+        if (fuel < km / consumption1){
+            return 0;
+        }
+        return fuel;
     }
 }
--- RomanAvdieiev/src/avdieiev/week1/TestCar.java
+++ RomanAvdieiev/src/avdieiev/week1/TestCar.java
@@ -1,10 +1,20 @@
 package avdieiev.week1;
 
 
+import java.util.Scanner;
+
 public class TestCar {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int km = sc.nextInt();
 
-        Car car = new Car ();
-        car.init("BMW", false, 30, 10000,"123");
+        Car car1 = new Car ();
+        car1.init("BMW", false, 30, 30000, "123", 10);
+
+//        String car1Info = car1.ShowAll(car1);
+//        System.out.println(car1Info);
+
+        Double car1Go = car1.go(km);
+        System.out.println(car1Go);
     }
 }
