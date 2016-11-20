490dce7b7121a094596f21ebb75c3b43cbe7e73a
Victoria <viktorialvov@gmail/com>
2016-11-20 11:08:47 +0200

    format code

--- VictoriaGitina/src/oop/week2/Car.java
+++ VictoriaGitina/src/oop/week2/Car.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+package oop.week2;
 
 /**
  * Created by Victoria on 11/19/2016.
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Car {
     public String model;
     public int fuel = 0;
-     public int price;
+    public int price;
     public boolean opened = true;
     public int keyOpen = 1111;
     public int keyClose = 1111;
@@ -37,10 +37,10 @@ public class Car {
     }
 
     public String showInfo() {
-        String str  = new String ("");
+        String str = new String("");
         //return fuel + " " + model;
-                str =  String.format("fuel %d, price %d, model %s, keyOpen %s",  fuel, price,  model, keyOpen, "keyClose %s", keyClose);
-        System.out.println (str);
+        str = String.format("fuel %d, price %d, model %s, keyOpen %s", fuel, price, model, keyOpen, "keyClose %s", keyClose);
+        System.out.println(str);
 
 
         return str;
