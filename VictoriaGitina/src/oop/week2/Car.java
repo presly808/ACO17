4eb9f8fd69227e48adf7cee73c614b2e59540cd1
Victoria <viktorialvov@gmail/com>
2016-11-24 00:42:58 +0200

    added getter,  setter, constructor

--- VictoriaGitina/src/oop/week2/Car.java
+++ VictoriaGitina/src/oop/week2/Car.java
@@ -4,21 +4,32 @@ package oop.week2;
  * Created by Victoria on 11/19/2016.
  */
 public class Car {
-    public String model;
-    public int fuel = 0;
-    public int price;
-    public boolean opened = true;
-    public int keyOpen = 1111;
-    public int keyClose = 1111;
+    private String model;
+    private int fuel;
+    private int price;
+    private boolean opened;
+    private int keyOpen;
+    private int keyClose;
+
+
+    public Car(String model) {
+        this.model = model; //    полю обякта присвою значення з параметра
+        this.fuel = 0;
+        this.opened = false;
+        this.keyOpen = 1111;
+        this.keyClose = 2222;
+    }
 
     public boolean open(int key) {
-
-        if (key == keyOpen) {
-            opened = true;
-        } else {
-            opened = false;
+        if (this.isOpened()) {
+            return this.opened;
         }
-        return opened;
+        if (key == this.keyOpen) {
+            this.opened = true;
+        } else {
+            this.opened = false;
+        }
+        return this.opened;
     }
 
     public boolean close(int key) {
@@ -37,15 +48,57 @@ public class Car {
         return true;
     }
 
-    public String showInfo() {
-        String str = new String("");
-        //return fuel + " " + model;
-        str = String.format("fuel %d, price %d, model %s, keyOpen %s", fuel, price, model, keyOpen, "keyClose %s", keyClose);
-      //  System.out.println(str);
-
-
-        return str;
+    public String getInfoString() {
+        return String.format("fuel %d, price %d, model %s, keyOpen %s, keyClose %s ",
+                fuel, price, model, keyOpen, keyClose);
     }
 
+    public String getModel() {
+        return model;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public int getKeyOpen() {
+        return keyOpen;
+    }
+
+    public int getKeyClose() {
+        return keyClose;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setOpened(boolean opened) {
+        this.opened = opened;
+    }
+
+    public void setKeyOpen(int keyOpen) {
+        this.keyOpen = keyOpen;
+    }
+
+    public void setKeyClose(int keyClose) {
+        this.keyClose = keyClose;
+    }
 }
 
