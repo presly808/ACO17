64600852059eecd941080a3443554cc59d0872a6
Victoria <viktorialvov@gmail/com>
2016-11-24 00:43:15 +0200

    added getter,  setter, constructor

---
+++ VictoriaGitina/src/oop/week2/CarShop.java
@@ -0,0 +1,19 @@
+package oop.week2;
+
+/**
+ * Created by Victoria on 11/20/2016.
+ */
+public class CarShop {
+
+    private String address;
+    private String name;
+    private int budget;
+
+
+
+//    public static Car showAll() {
+//
+   }
+//}
+//
+//public boolean addcars ////ADD THIS METHOD
